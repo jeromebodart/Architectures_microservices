2b4e6d7cdcff4aa20e96fd0ec1e24fc56b8853fc
jeromebodart <jerome.bodart@hei.yncrea.fr>
2019-05-14 16:01:55 +0200

    Fix eureka and start Ribbon

--- microservice-produits/src/main/java/com/mproduits/MproduitsApplication.java
+++ microservice-produits/src/main/java/com/mproduits/MproduitsApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableConfigurationProperties
 @EnableDiscoveryClient
+@EnableEurekaClient
 public class MproduitsApplication {
 
 	public static void main(String[] args) {
