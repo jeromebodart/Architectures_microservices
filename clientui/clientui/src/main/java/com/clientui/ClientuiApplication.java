de70f94519c3ae3d5d4034b7870a718547414056
jeromebodart <jerome.bodart@hei.yncrea.fr>
2019-05-15 14:51:40 +0200

    Spring security

--- clientui/clientui/src/main/java/com/clientui/ClientuiApplication.java
+++ clientui/clientui/src/main/java/com/clientui/ClientuiApplication.java
@@ -2,10 +2,14 @@ package com.clientui;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients("com.clientui")
+@EnableConfigurationProperties
+@EnableEurekaClient
 public class ClientuiApplication {
 
 	public static void main(String[] args) {
